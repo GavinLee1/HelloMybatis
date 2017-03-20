40f60a3656051c6f5866eb89994a929e3acdb738
ligaozhao <ligaozhao@mozat.com>
2017-03-20 11:03:13 +0800

    Add API for getUserById

--- src/main/java/com/hello/mybatis/controller/UserController.java
+++ src/main/java/com/hello/mybatis/controller/UserController.java
@@ -1,7 +1,25 @@
 package com.hello.mybatis.controller;
 
+import com.hello.mybatis.service.interf.UserService;
+import com.hello.mybatis.service.message.GetUserResp;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+
 /**
  * Created by ligaozhao on 19/03/17.
  */
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @Inject
+    private UserService userService;
+
+    @ResponseBody
+    @RequestMapping(value = "/count-account-by-name", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public GetUserResp countAccountByName(@RequestParam int id) {
+        return userService.getUserById(id);
+    }
 }
